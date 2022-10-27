133a1f119adc9d57151a5677cb7b278683a7e970
Leidi Flores <floresreyesleidi@gmail.com>
2022-10-27 10:55:36 -0500

    fix script ts-particles

--- glitter/js/404.js
+++ glitter/js/404.js
@@ -2,33 +2,33 @@ var particles = {
     fpsLimit: 160,
     particles: {
         number: {
-            value: 400,
+            value: 500,
             density: {
                 enable: true,
-                area: 400
+                area: 100
             }
         },
         color: {
-            value: "#ffffff"
+            value: "#fff009"
         },
         shape: {
             type: "star"
         },
         opacity: {
-            value: 1,
+            value: 0.91,
             random: {
                 enable: true,
                 minimumValue: 0.1
             },
             animation: {
                 enable: true,
-                speed: 1,
-                minimumValue: 0,
+                speed: 10,
+                minimumValue: 60,
                 sync: false
             }
         },
         size: {
-            value: 2,
+            value: 3,
             random: {
                 enable: true,
                 minimumValue: 1
@@ -36,22 +36,12 @@ var particles = {
         },
         move: {
             enable: true,
-            speed: 0.17,
+            speed: 1,
             direction: "none",
             random: true,
-            straight: false,
-            outModes: {
-                default: "out"
-            },
+            straight: true
         }
-    },
-    interactivity: {
-        detectsOn: "canvas",
-        events: {
-            resize: false
-        }
-    },
-    detectRetina: true
+    }
 };
 
 tsParticles.load("tsparticles", particles);
